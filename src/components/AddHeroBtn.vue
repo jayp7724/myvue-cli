47fd23088959f3cd2b60d87d96a71762cc61af81
Jay Patel <Jayp7724@gmail.com>
2021-11-30 21:41:50 +0000

    done

---
+++ src/components/AddHeroBtn.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "add-hero-btn",
+    props: {
+        text: String,
+    }
+}
+</script>
+
+<template>
+    <a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue" href="#0" @click="$emit('add-hero', true)">{{ text }}</a>
+</template>
